0503bb48a5041be4d0db1da14fed05c89ea737bd
Nauman Khalid <wp061005@gmail.com>
2022-02-14 22:47:40 -0500

    Adding and removing the tasks from To Do list in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'To Do List';
+  list:any[]=[];
+  addTask(item:string){
+this.list.push({id:this.list.length, name:item});
+
+  }
+  removeTask(id:number){
+this.list=this.list.filter(item=>item.id!==id)
+  }
 }
